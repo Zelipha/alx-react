9ccb4fa9c8e908d82efe1442429b7662631b4f07
Zelipha <zeliphawambui756@gmail.com>
2022-07-11 20:35:42 +0300

    Modified file

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
   module: {
     rules: [
       {
