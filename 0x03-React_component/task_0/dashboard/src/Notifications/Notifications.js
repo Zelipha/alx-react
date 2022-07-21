a0b037599f1d21c984a8bab09a7a0dfea22924b3
Zelipha <zeliphawambui756@gmail.com>
2022-07-21 14:44:39 +0300

    Commence with class components

--- 0x03-React_component/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_0/dashboard/src/Notifications/Notifications.js
@@ -7,49 +7,42 @@ import NotificationItemShape from "./NotificationItemShape";
 
 function Notifications({ displayDrawer, listNotifications }) {
   return (
-    <React.Fragment>
+    <>
+      <div className="menuItem">
+        <p>Your notifications</p>
+      </div>
       {displayDrawer ? (
-        <div className="'flex-area'">
-          <div className="menuItem">
-            <p>Your notifications</p>
-          </div>
-          <div className="Notifications">
-            {/* <p>Here is the list of notifications</p> */}
-            <ul>
-              {listNotifications && listNotifications.length > 0 ? (
-                listNotifications.map(({ id, html, type, value }) => (
-                  <>
-                    <NotificationItem key={id} type={type} value={value} html={html} />
-                    {/* <button
-                      style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "10px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
-                      aria-label="Close"
-                      onClick={console.log("Close button has been clicked")}
-                    >
-                      <img src={closeIcon} alt="closeIcon" width="10px" />
-                    </button> */}
-                  </>
-                ))
-              ) : (
-                <div className="">
-                  <NotificationItem value="No new notification for now" />
-                  <button
-                    style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "10px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
-                    aria-label="Close"
-                    onClick={console.log("Close button has been clicked")}
-                  >
-                    <img src={closeIcon} alt="closeIcon" width="10px" />
-                  </button>
-                </div>
-              )}
-            </ul>
-          </div>
+        <div className="Notifications">
+          <button
+            style={{
+              color: "#3a3a3a",
+              fontWeight: "bold",
+              background: "none",
+              border: "none",
+              fontSize: "15px",
+              position: "absolute",
+              right: "3px",
+              top: "3px",
+              cursor: "pointer",
+              outline: "none",
+            }}
+            aria-label="Close"
+            onClick={(e) => {
+              console.log("Close button has been clicked");
+            }}
+          >
+            <img src={closeIcon} alt="close icon" width="10px" />
+          </button>
+          {listNotifications.length != 0 ? <p>Here is the list of notifications</p> : null}
+          <ul>
+            {listNotifications.length == 0 ? <NotificationItem type="default" value="No new notification for now" /> : null}
+            {listNotifications.map((val, idx) => {
+              return <NotificationItem type={val.type} value={val.value} html={val.html} key={val.id} />;
+            })}
+          </ul>
         </div>
-      ) : (
-        <div className="menuItem">
-          <p>Your notifications</p>
-        </div>
-      )}
-    </React.Fragment>
+      ) : null}
+    </>
   );
 }
 
