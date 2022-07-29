0a3628e7519c02e528d5dffd0fd8425c85e55de5
Zelipha <zeliphawambui756@gmail.com>
2022-07-29 20:38:33 +0300

    Install Aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
@@ -48,7 +48,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <div className={css(styles.App)}>
-          <div className="heading-section">
+          <div className={css(styles["heading-section"])}>
             <Notifications listNotifications={this.listNotifications} />
             <Header />
           </div>
@@ -80,6 +80,19 @@ const styles = StyleSheet.create({
     maxWidth: "100vw",
     position: "relative",
     fontFamily: "Arial, Helvetica, sans-serif",
+  },
+  "heading-section": {
+    borderBottom: "4px solid red",
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row-reverse",
+  },
+  "App-footer": {
+    borderTop: "4px solid red",
+    fontSize: "1.4rem",
+    padding: "0.5em",
+    textAlign: "center",
+    fontStyle: "italic",
   },
 });
 
