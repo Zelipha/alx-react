8e58fc89105a64529a3363b60fbd73201c09313b
Zelipha <zeliphawambui756@gmail.com>
2022-08-05 02:53:33 +0300

    Chained mutations

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -10,5 +10,5 @@ export const map = Map({
 });
 
 export const map2 = map.withMutations((values) => {
-	values.set(2, 'Benjamin').set(4, 'Oliver');
+  values.set(2, 'Benjamin').set(4, 'Oliver');
 });
