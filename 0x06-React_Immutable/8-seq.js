865360a40708e9762c3ecf6286b4a3d6b315c6ea
Zelipha <zeliphawambui756@gmail.com>
2022-08-05 03:12:20 +0300

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,15 +1,24 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(grades) {
-  const bestStudents = Seq(grades)
-    .filter((student) => student.score >= 70)
-    .map((student) => ({
-      score: x.score,
-      firstName:
-        student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
-      lastName:
-	student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
-    }));
+export default function printBestStudents(object) {
+  const seq = Seq(object);
 
-  console.log(bestStudents.toObject());
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
+  });
+
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
 }
